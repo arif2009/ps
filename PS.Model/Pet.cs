cf8c703b536505cb858c29a47bdd9dbd2a3f0eb3
Arif <arif.rahman2009@gmail.com>
2019-08-17 21:28:41 +0600

    Update model

---
+++ PS.Model/Pet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PS.Model
+{
+    public class Pet
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ChipIdentifier { get; set; }
+        public DateTime InplanteDate { get; set; }
+        public long Price { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ PS.Model/User.cs
@@ -0,0 +1,14 @@
+﻿using PS.Utility;
+using System;
+
+namespace PS.Model
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string SSN { get; set; } //SSN = Social Security Number
+        public Roles Role { get; set; } = Roles.Customer; // Set Customer as a default role
+    }
+}
