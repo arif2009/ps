15b6a24cee52b9b9f6c831a9a5ffc0ac01959e76
Arif <arif.rahman2009@gmail.com>
2019-08-18 17:31:36 +0600

    Add pet manager

--- PS.Manager/IPetManager.cs
+++ PS.Manager/IPetManager.cs
@@ -7,5 +7,10 @@ namespace PS.Manager
     public interface IPetManager
     {
         List<Pet> GetAllPets();
+        List<Pet> GetSoldPets();
+        List<Pet> GetUnSoldablePets();
+        List<Pet> GetDogs();
+        List<Pet> GetCats();
+        List<Pet> GetBirds();
     }
 }
---
+++ PS.Manager/PetManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using PS.Domain;
+using PS.Model;
+using PS.Utility;
+
+namespace PS.Manager
+{
+    public class PetManager : IPetManager
+    {
+        private IPetRepository _petRepository;
+
+        public PetManager(IPetRepository petRepository)
+        {
+            _petRepository = petRepository;
+        }
+
+        public List<Pet> GetAllPets()
+        {
+            return _petRepository.GetAllPets();
+        }
+
+        public List<Pet> GetBirds()
+        {
+            return this.GetUnSoldablePets().FindAll(x=>x.PetType == PetTypes.Bird);
+        }
+
+        public List<Pet> GetCats()
+        {
+            return this.GetUnSoldablePets().FindAll(x => x.PetType == PetTypes.Cat);
+        }
+
+        public List<Pet> GetDogs()
+        {
+            return this.GetUnSoldablePets().FindAll(x => x.PetType == PetTypes.Dog);
+        }
+
+        public List<Pet> GetSoldPets()
+        {
+            return _petRepository.GetAllPets().FindAll(x => x.IsSold);
+        }
+
+        public List<Pet> GetUnSoldablePets()
+        {
+            return _petRepository.GetAllPets().FindAll(x => !x.IsSold);
+        }
+    }
+}
