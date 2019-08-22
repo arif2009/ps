dceb895048637f5b6d619a2fc3cf129c898f1a50
Arif <arif.rahman2009@gmail.com>
2019-08-22 09:52:07 +0600

    Added service

---
+++ PS.Service/RevenueService.cs
@@ -0,0 +1,20 @@
+﻿using PS.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Service
+{
+    public class RevenueService
+    {
+        private IPetManager _petManager;
+        private ITransManager _transManager;
+        public RevenueService(IPetManager petManager, ITransManager transManager)
+        {
+            _petManager = petManager;
+            _transManager = transManager;
+        }
+    }
+}
