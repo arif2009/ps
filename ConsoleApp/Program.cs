5d4593c6eb8108ed5f26782f7180640001e72ef7
Arif <arif.rahman2009@gmail.com>
2019-08-26 09:53:55 +0600

    Separate week

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,11 +2,13 @@
 using PS.Domain;
 using PS.Manager;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PS.Service;
+using System.Globalization;
 
 namespace ConsoleApp
 {
@@ -18,7 +20,31 @@ namespace ConsoleApp
             container = ConfigureContainer();
             var revinueService = container.Resolve<IRevenueService>();
 
-            var allUser = revinueService.GetRevinueBetween(DateTime.Now, DateTime.Now.AddDays(5));
+            DateTime reference = DateTime.Now;
+            Calendar calendar = CultureInfo.CurrentCulture.Calendar;
+
+            IEnumerable<int> daysInMonth = Enumerable.Range(1, DateTime.Now.Day);
+
+            var a = daysInMonth.Select(day => new DateTime(reference.Year, reference.Month, day));
+            var b = a.GroupBy(d => calendar.GetWeekOfYear(d, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday));
+            //var hd = b.Where(x => x.DayOfWeek == DayOfWeek.Sunday);
+            //var c = b.Except();
+            /*List<Tuple<DateTime, DateTime>> weeks = daysInMonth.Select(day => new DateTime(reference.Year, reference.Month, day))
+                .GroupBy(d => calendar.GetWeekOfYear(d, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday))
+                .Select(g => new Tuple<DateTime, DateTime>(g.First(), g.Last()))
+                .ToList();*/
+
+            var weeks = daysInMonth.Select(day => new DateTime(reference.Year, reference.Month, day))
+                .GroupBy(d => calendar.GetWeekOfYear(d, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday))
+                .Select(g => new
+                {
+                    g.First()
+                })
+                .ToList();
+
+            //weeks.ForEach(x => Console.WriteLine("{0:MM/dd/yyyy} - {1:MM/dd/yyyy}", x.Item1, x.Item2));
+
+            //var allUser = revinueService.GetRevinueBetween(DateTime.Now, DateTime.Now.AddDays(5));
 
             Console.ReadKey();
         }
